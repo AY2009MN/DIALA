061b66a55b59544837e781a122effcf8155f27d3
AY2009MN <36678306+AY2009MN@users.noreply.github.com>
2026-05-31 23:29:30 +0300

    إصدار أولي - اختبار الرياضيات ديالا

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'math-quiz-v2';
+const URLS = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(URLS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request).catch(() => caches.match('/index.html')))
+  );
+});
